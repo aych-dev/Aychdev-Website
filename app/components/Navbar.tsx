a2baf8f55c79a7416506479451b6174e420c3c6f
aych <aychdev1@gmail.com>
2023-10-25 14:10:46 -0400

    update

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className='text-white py-4 '>
+      <div className='container mx-auto flex justify-between items-center'>
+        <Link href='/' className='text-xl font-bold'>
+          Aychdev
+        </Link>
+        <div className='flex space-x-4'>
+          <a
+            href='https://resume.creddle.io/resume/dmxtfydshbx'
+            target='_blank'
+          >
+            Resume
+          </a>
+          <Link href='/projects'>Projects</Link>
+          <a
+            href='https://www.linkedin.com/in/henry-dominguez-856937260/'
+            target='_blank'
+          >
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
