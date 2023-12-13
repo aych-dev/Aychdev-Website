cdf5abcab67fb344ddf1eb9ca6e9d73f73e07169
aych <aychdev1@gmail.com>
2023-12-12 19:00:38 -0500

    updated intro

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -11,13 +11,13 @@ const Introduction = () => {
           <p className='m-4 '>
             My name is Henry Dominguez and I go by the alias Aychdev (eych-dev)
             online. I am a self-taught developer interested in all things
-            blockchain and web/app development. I am currently learning how to
-            implement Solana blockchain applications on the front-end and have
-            an interest in soon learning Rust. I love that my work feels
-            meaningful and I really enjoy helping up-and-coming aspiring
-            developers. More than anything, I want my story to inspire people to
-            believe in their potential regardless of what they might feel or
-            hear from others. Do not hesitate to reach out!
+            blockchain and web/app development. It was not until I got older
+            that I grew a huge interest in coding/development so I hope my story
+            inspires anyone to go for it. I love that my work feels meaningful
+            and I really enjoy helping up-and-coming aspiring developers. More
+            than anything, I want my story to inspire people to believe in their
+            potential regardless of what they might feel or hear from others. Do
+            not hesitate to reach out!
           </p>
         </div>
         <div className='border border-white mt-5 '>
