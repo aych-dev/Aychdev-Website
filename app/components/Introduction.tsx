1fc088ac8af6ec49b6e60e2b379b2aeac018f81b
aych <aychdev1@gmail.com>
2023-12-19 18:42:25 -0500

    updated intro

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -10,12 +10,12 @@ const Introduction = () => {
           </div>
           <p className='m-4 '>
             My name is Henry Dominguez and I go by the alias Aychdev (eych-dev)
-            online. I am a self-taught developer interested in all things
-            blockchain and web/app development. It was not until I got older
-            that I grew a huge interest in coding/development so I hope my story
-            inspires anyone to go for it. I love that my work feels meaningful
-            and I really enjoy helping up-and-coming aspiring developers. More
-            than anything, I want my story to inspire people to believe in their
+            online. I am a software engineer interested in all things blockchain
+            and web/app development. It was not until I got older that I grew a
+            huge interest in coding/development so I hope my story inspires
+            anyone to go for it. I love that my work feels meaningful and I
+            really enjoy helping up-and-coming aspiring developers. More than
+            anything, I want my story to inspire people to believe in their
             potential regardless of what they might feel or hear from others. Do
             not hesitate to reach out!
           </p>
