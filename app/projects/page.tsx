6c7d4c57b55d775a050296e33b03874d3dad4368
aych <aychdev1@gmail.com>
2024-01-19 23:08:56 -0500

    update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -71,7 +71,7 @@ const Projects = (): JSX.Element => {
           </div>
           <div className='mt-4 px-2'>
             <h2 className='text-xl font-semibold'>{project.name}</h2>
-            <p className='mt-2 md:text-sm sm:text-xs'>{project.description}</p>
+            <p className='mt-2 md:text-sm sm:text-s'>{project.description}</p>
           </div>
           <div className='flex items-center justify-center font-semibold mt-2 '>
             <a
